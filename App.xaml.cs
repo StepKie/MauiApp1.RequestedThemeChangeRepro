67c86a2201b26275d2cb214518385cd27f83e395
Stephan Kieburg <s.kieburg@jpsc.de>
2023-05-31 10:47:47 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		RequestedThemeChanged += (s, a) => ConfigureTheme(a.RequestedTheme);
+		InitializeComponent();
+		MainPage = new AppShell();
+	}
+
+	private void ConfigureTheme(AppTheme requestedTheme)
+	{
+		// Breakpoint set on this line will only be hit exactly ONCE (the first time the system theme changes)
+		UserAppTheme = requestedTheme;
+	}
+}
